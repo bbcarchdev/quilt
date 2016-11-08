22bb29685630db375f969be08fdd78a1c53e3559
Christophe Gueret <christophe.gueret@bbc.co.uk>
2016-11-08 14:11:33 +0000

    Remove specific docker files now that Acropolis is built as a single image

--- docker/cucumber/features/support/env.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-
-require 'capybara/cucumber'
-require 'capybara/poltergeist'
-Capybara.configure do |c|
-  c.javascript_driver = :poltergeist
-  c.default_driver = :poltergeist
-end
